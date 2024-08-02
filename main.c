cb2ff5426a907457561cac665f659137fa4cd748
Daruuu <thedaruny16@gmail.com>
2024-08-02 18:45:49 +0200

    modi main

--- main.c
+++ main.c
@@ -6,14 +6,21 @@
 /*   By:  dasalaza < dasalaza@student.42barcelon    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 23:39:48 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/08/01 13:04:25 by  dasalaza        ###   ########.fr       */
+/*   Updated: 2024/08/02 18:44:42 by  dasalaza        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	main(void)
+int	main(int ac, char **av)
 {
 	printf("Hello, World!\n");
+
+	if (ac == 2)
+	{
+		// comprobar que el av[1] sea un mapa.ber
+
+	}
+
 	return (0);
 }
